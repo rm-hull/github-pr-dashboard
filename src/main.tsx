48618dade07f62d7f087b04e2afb4a531c34646e
Richard Hull <rm_hull@yahoo.co.uk>
2025-10-25 21:53:55 +0100

    Add GlitchTip/Sentry error tracking

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,28 @@
+import * as Sentry from "@sentry/react";
 import { createRouter, RouterProvider } from "@tanstack/react-router";
-import { createRoot } from "react-dom/client";
+import { createRoot, RootOptions } from "react-dom/client";
 import { AppProviders } from "./components/AppProviders";
 import { routeTree } from "./routeTree.gen";
 
+if (import.meta.env.VITE_SENTRY_DSN) {
+  Sentry.init({
+    dsn: import.meta.env.VITE_SENTRY_DSN as string,
+    release: import.meta.env.VITE_GIT_COMMIT_HASH as string,
+    environment: import.meta.env.MODE,
+  });
+}
+
 const router = createRouter({ routeTree });
 
-createRoot(document.getElementById("root")!).render(
+const errorHandling: RootOptions = {
+  onUncaughtError: Sentry.reactErrorHandler((error, errorInfo) => {
+    console.warn("Uncaught error", error, errorInfo.componentStack);
+  }),
+  onCaughtError: Sentry.reactErrorHandler(),
+  onRecoverableError: Sentry.reactErrorHandler(),
+};
+
+createRoot(document.getElementById("root")!, errorHandling).render(
   <AppProviders>
     <RouterProvider router={router} />
   </AppProviders>
