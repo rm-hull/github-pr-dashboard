639ebeab61f96c0c9f9543348a31123ac4516656
Richard Hull <rm_hull@yahoo.co.uk>
2025-10-25 14:59:59 +0100

    Force reload when GH token expires

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { RequestError } from "@octokit/request-error";
+import { QueryCache, QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { createRouter, RouterProvider } from "@tanstack/react-router";
 import { StrictMode } from "react";
@@ -7,7 +8,21 @@ import { Provider as ChakraProvider } from "./components/ui/provider";
 import { Toaster } from "./components/ui/toaster";
 import { routeTree } from "./routeTree.gen";
 
-const queryClient = new QueryClient();
+function isRequestError(error: unknown): error is RequestError {
+  return error instanceof RequestError;
+}
+
+const queryClient = new QueryClient({
+  queryCache: new QueryCache({
+    onError: (error: unknown) => {
+      if (isRequestError(error) && error.status === 401) {
+        sessionStorage.removeItem("gh_token");
+        window.location.reload();
+      }
+    },
+  }),
+});
+
 const router = createRouter({ routeTree });
 
 createRoot(document.getElementById("root")!).render(
